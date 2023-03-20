4ca17757c9b8456470b5aeabb35daa9015857d03
annep88 <65869092+annep88@users.noreply.github.com>
2023-03-19 22:11:20 -0400

    Create index.php

---
+++ web250/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang = "en">
+<?php $title = 'WEB250 ~ Anna Winter\'s Awesome Walrus ~ Home'; include("header.php");?>
+<h3>Welcome to Anna's Home Page</h3>
+</main>
+<?php include("footer.php");?>
+</body>
+</html>
