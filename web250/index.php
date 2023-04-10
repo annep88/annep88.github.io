a82b25af91795c3921b6591a74c19e21d9d10d4c
annep88 <65869092+annep88@users.noreply.github.com>
2023-04-09 21:33:56 -0400

    deleted php files

--- web250/index.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang = "en">
-<?php $title = 'WEB250 ~ Anna Winter\'s Awesome Walrus ~ Home'; include("header.php");?>
-<h3>Welcome to Anna's Home Page</h3>
-</main>
-<?php include("footer.php");?>
-</body>
-</html>
